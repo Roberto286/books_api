720262941d85255ff5b6e6fe44c175147620e602
Roberto286 <roberto.saliola96@gmail.com>
2023-08-16 13:36:37 +0200

    created database helper class

---
+++ src/lib/database.js
@@ -0,0 +1,45 @@
+const mysql = require('mysql2');
+const { host, user, password, database } = require('../../config/database.json');
+
+class Database {
+ pool;
+
+ constructor() {
+  this.pool = mysql.createPool({
+   host: host,
+   user: user,
+   password: password,
+   database: database,
+   waitForConnections: true,
+   connectionLimit: 10,
+   maxIdle: 10,
+   idleTimeout: 60000,
+   queueLimit: 0,
+   enableKeepAlive: true,
+   keepAliveInitialDelay: 0,
+  });
+ }
+
+ getAllBooks(callback_success, callback_error) {
+  const sql = 'SELECT * FROM view_books';
+  this.pool.execute(sql, (err, res) => {
+   if (err) {
+    callback_error(err);
+   } else {
+    callback_success(res);
+   }
+  });
+ }
+
+ getBookBy(column, columnValue, callback_success, callback_error) {
+  const sql = `SELECT * FROM view_books WHERE ${column} = ?`;
+  this.pool.execute(sql, [columnValue], (err, res) => {
+   if (err) {
+    callback_error(err);
+   } else {
+    callback_success(res[0]);
+   }
+  });
+ }
+}
+module.exports = Database;
